5d1d823b0a92fd7e097ae6d6af35e8f99b8d5b17
jinseokoh <jinseok@pineone.com>
2023-02-21 16:38:48 +0900

    Spring Batch 기능 활성화

--- src/main/java/com/tutorial/batch/SpringBatchTutorialApplication.java
+++ src/main/java/com/tutorial/batch/SpringBatchTutorialApplication.java
@@ -1,8 +1,10 @@
 package com.tutorial.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing  // Spring Batch 기능 활성화
 @SpringBootApplication
 public class SpringBatchTutorialApplication {
 
