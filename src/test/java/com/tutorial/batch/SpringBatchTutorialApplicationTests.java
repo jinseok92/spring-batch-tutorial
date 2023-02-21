ca9cc6a8953a525717fa310bbb3f0a7e491abd16
jinseokoh <jinseok@pineone.com>
2023-02-21 15:43:12 +0900

    Initial commit

---
+++ src/test/java/com/tutorial/batch/SpringBatchTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
