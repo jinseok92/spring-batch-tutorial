256f4ddb66ce40f274fd9ef7fafe166bb148e88c
jinseokoh <jinseok@pineone.com>
2023-02-22 18:02:34 +0900

    JobInstance commit

--- src/main/java/com/tutorial/batch/JobInstanceConfiguration.java
+++ src/main/java/com/tutorial/batch/JobInstanceConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class JobConfiguration {
+public class JobInstanceConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
