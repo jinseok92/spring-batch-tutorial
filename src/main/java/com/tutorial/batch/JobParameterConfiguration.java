c440c34fcd2ade3b84d913799abd85c1c6a4742a
jinseokoh <jinseok@pineone.com>
2023-02-23 09:11:58 +0900

    JobParameter commit

--- src/main/java/com/tutorial/batch/JobParameterConfiguration.java
+++ src/main/java/com/tutorial/batch/JobParameterConfiguration.java
@@ -1,7 +1,9 @@
 package com.tutorial.batch;
 
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -11,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class JobConfiguration {
+public class JobParameterConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -28,8 +30,11 @@ public class JobConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
             .tasklet((contribution, chunkContext) -> {
+                JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
+
                 System.out.println("=============================");
                 System.out.println("Step1 has executed");
+                System.out.println("jobParameters = " + jobParameters);
                 System.out.println("=============================");
                 return RepeatStatus.FINISHED;
             })
@@ -40,8 +45,11 @@ public class JobConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
             .tasklet((contribution, chunkContext) -> {
+                Map<String, Object> jobParameters = chunkContext.getStepContext().getJobParameters();
+
                 System.out.println("=============================");
                 System.out.println("Step2 has executed");
+                System.out.println("jobParameters = " + jobParameters);
                 System.out.println("=============================");
                 return RepeatStatus.FINISHED;
             })
