961a5f953104d6e9f9fc1d8356a55fec6084312d
jinseokoh <jinseok@pineone.com>
2023-03-15 17:08:45 +0900

    test

---
+++ src/main/java/com/tutorial/batch/JobScheduler.java
@@ -0,0 +1,40 @@
+package com.tutorial.batch;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class JobScheduler {
+
+    private final JobLauncher jobLauncher;
+    private final JobConfiguration jobConfiguration;
+
+    @Scheduled(cron = "0 * * * * *")
+    public void jobScheduled()
+        throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+        Date date = new Date();
+        sdf.format(date);
+        System.out.println("date = " + date);
+
+        Map<String, JobParameter> jobParametersMap = new HashMap<>();
+        jobParametersMap.put("date", new JobParameter(date));
+        JobParameters parameters = new JobParameters(jobParametersMap);
+
+        jobLauncher.run(jobConfiguration.job(), parameters);
+
+    }
+}
