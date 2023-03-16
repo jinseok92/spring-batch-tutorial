33bb3689fcb76a90e9eb059b495870ce4f421355
jinseokoh <jinseok@pineone.com>
2023-03-16 12:33:03 +0900

    test

--- src/main/java/com/tutorial/batch/JobConfiguration.java
+++ src/main/java/com/tutorial/batch/JobConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,37 +21,11 @@ public class JobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    /**
-     * FlowJob 예제
-     * step1 성공 시 : step1 -> step2 -> step3
-     * step1 실패 시 : step1 -> step3
-     * @return
-     */
     @Bean
-    public Job flowJob() {
-        return jobBuilderFactory.get("flowJob")
-            // step1 실행
+    public Job job() {
+        return jobBuilderFactory.get("job")
             .start(step1())
-                // step1 실행 결과가 FAILED 일 경우
-                .on("FAILED")
-                // step3 실행
-                .to(step3())
-                // step3 실행 결과에 상관없이 Flow을 종료한다.
-                .on("*")
-                .end()
-            // step1 으로 부터
-            .from(step1())
-                // step1 실행 결과가 FAILED 외 모든 경우
-                .on("*")
-                // step2 실행
-                .to(step2())
-                // step3 실행
-                .next(step3())
-                // step3 실행 결과 상관없이 Flow 종료
-                .on("*")
-                .end()
-            // Job 종료
-            .end()
+            .next(step2())
             .build();
     }
 
@@ -61,11 +37,9 @@ public class JobConfiguration {
                 System.out.println("Step1 has executed");
                 System.out.println("=============================");
 
-                // ExitStatus 를 FAILED 로 지정
-//                contribution.setExitStatus(ExitStatus.FAILED);
-
                 return RepeatStatus.FINISHED;
             })
+            .allowStartIfComplete(true)
             .build();
     }
 
@@ -76,20 +50,12 @@ public class JobConfiguration {
                 System.out.println("=============================");
                 System.out.println("Step2 has executed");
                 System.out.println("=============================");
-                return RepeatStatus.FINISHED;
+                throw new RuntimeException();
+
+//                return RepeatStatus.FINISHED;
             })
+            .startLimit(3)
             .build();
     }
 
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-            .tasklet((contribution, chunkContext) -> {
-                System.out.println("=============================");
-                System.out.println("Step3 has executed");
-                System.out.println("=============================");
-                return RepeatStatus.FINISHED;
-            })
-            .build();
-    }
 }
